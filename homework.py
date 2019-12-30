2fd68a79b18843f40992c4b3f1dd192c1065debb
Алексей <leha-spb@yandex.ru>
2019-12-30 10:22:53 +0300

    Second hw_python_oop

--- homework.py
+++ homework.py
@@ -6,30 +6,22 @@ class Calculator():
         self.records = []
         
     def add_record(self, record):
-        self.record = record
         self.records.append(record)
         return self.records
     
     def get_today_stats(self):
-        total_day = 0
-        for i in self.records:
-            if i.date == dt.date.today():
-                total_day += i.amount
-        return total_day
-    
+        return sum([record.amount for record in self.records if record.date == dt.date.today()])
+      
     def get_week_stats(self):
-        total_week = 0
         last_week = dt.date.today() - dt.timedelta(weeks=1)
-        for i in self.records:
-            if last_week <= i.date <= dt.date.today():
-                total_week += i.amount
-        return total_week
+        return sum([record.amount for record in self.records if last_week <= record.date <= dt.date.today()])
+        
 
 class Record:
     def __init__ (self, amount, comment, date = dt.date.today()):
         self.amount = amount
         self.comment = comment
-        if (type(date)==str):
+        if isinstance(date, str):
             self.date = dt.datetime.strptime(date,'%d.%m.%Y').date()
         else:
             self.date = date
@@ -39,33 +31,29 @@ class CaloriesCalculator(Calculator):
     def get_calories_remained(self):    
         if self.get_today_stats() < self.limit:
             return f'Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более {self.limit - self.get_today_stats()} кКал'
-        else:
-            return 'Хватит есть!'
+        return 'Хватит есть!'
             
 class CashCalculator(Calculator):
     USD_RATE = 60.00
-    EURO_RATE = 70.00
+    EURO_RATE = 70.00 
+    
     def get_today_cash_remained(self, currency):
-        self.currency = currency
         count = self.limit - self.get_today_stats()
-        if count == 0:
+        
+        cash_dict = {
+        "rub" : ('руб', 1),
+        "usd" : ('USD', self.USD_RATE),
+        "eur" : ('Euro', self.EURO_RATE)
+        }
+
+        total = abs(count / cash_dict[currency][1])
+
+        if count > 0:
+            return f'На сегодня осталось {int(total) if currency == "rub" else round(total, 2)} {cash_dict[currency][0]}'
+        elif count == 0:
             return 'Денег нет, держись'      
         else:
-            if self.currency == "rub":
-                if count > 0:
-                    return f'На сегодня осталось {count} руб'
-                else:
-                    return f'Денег нет, держись: твой долг - {abs(count)} руб'
-            elif self.currency == "usd":
-                if count > 0:
-                    return f'На сегодня осталось {round(count / self.USD_RATE, 2)} USD'
-                else:
-                    return f'Денег нет, держись: твой долг - {round(abs(count) / self.USD_RATE, 2)} USD'
-            else:
-                if count > 0:
-                    return f'На сегодня осталось {round(count / self.EURO_RATE, 2)} Euro'
-                else:
-                    return f'Денег нет, держись: твой долг - {round(abs(count) / self.EURO_RATE, 2)} Euro'
+            return f'Денег нет, держись: твой долг - {int(total) if currency == "rub" else round(total, 2)} {cash_dict[currency][0]}'
 
 
 
